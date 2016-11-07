a976aff91da9641366e12f5e4149b8f384ea5d41
Rene Saare <rene@url.ee>
2016-11-07 14:35:20 +0200

    Better data provider implementation

--- tests/AbstractImageTest.php
+++ tests/AbstractImageTest.php
@@ -129,11 +129,16 @@ abstract class AbstractImageTest extends TestCase
     {
         // [width, height, keepAspectRatio, allowUpscaling, newWidth, newHeight]
         return [
-            [350, 350, true, false, 350, 180],    // Height and width set. Image should keep aspect ratio.
-            [350, 350, false, false, 350, 350],   // Height and width set. Image should be resized to exact dimensions.
-            [350, null, true, false, 350, 180],   // Height omitted and is calculated based on original image aspect ratio.
-            [null, 180, true, false, 350, 180],   // Width omitted and is calculated based on original image aspect ratio.
-            [800, 800, true, true, 800, 411],     // Upscaling
+            'Height and width set. Image should keep aspect ratio.' =>
+                [350, 350, true, false, 350, 180],
+            'Height and width set. Image should be resized to exact dimensions.' =>
+                [350, 350, false, false, 350, 350],
+            'Height omitted and is calculated based on original image aspect ratio.' =>
+                [350, null, true, false, 350, 180],
+            'Width omitted and is calculated based on original image aspect ratio.' =>
+                [null, 180, true, false, 350, 180],
+            'Upscaling' =>
+                [800, 800, true, true, 800, 411],
         ];
     }
 
