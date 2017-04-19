b89ac3fcc311442e3e73fa14a7bf7293fda704de
Alexander Makarov <sam@rmcreative.ru>
2017-04-19 18:22:38 +0300

    Fixed PHPUnit6 compatibility

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,3 +13,5 @@ require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@yiiunit/extensions/imagine', __DIR__);
 Yii::setAlias('@yii/imagine', dirname(__DIR__));
+
+require_once(__DIR__ . '/compatibility.php');
